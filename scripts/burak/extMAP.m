f693030edcb09eafd50f452e896021e7b8b43fc4
Catherine Walsh <cwalsh7894@gmail.com>
2024-11-05 14:49:41 -0500

    add burak's scripts

---
+++ scripts/burak/extMAP.m
@@ -0,0 +1,20 @@
+clear;%load MAP save MAP
+addpath(genpath('/data/SFIM/akin/bin/burak'));
+addpath(genpath('/data/SFIM/akin/bin/NIfTI_20140122'));
+basepath=['/data/akinb2/allbp/'];
+cc=0; clear mapmat
+for vv=10:13
+    cc=cc+1;
+fullname=strcat(basepath,'bp',num2str(vv,'%.02d'),'/','vitalsnew.mat');
+load(fullname);
+    mapmat(cc,:)=mapin_fmri;
+    
+end
+    
+     for vv=[14 15 16  18 19 20 21];
+cc=cc+1;
+fullname=strcat(basepath,'bp',num2str(vv,'%.02d'),'/','vitals.mat');
+load(fullname);
+mapmat(cc,:)=mapin_fmri;
+     end
+
