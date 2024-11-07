35b078a77cae54b2376036d1e113eddf5c1a3ab1
Josh Dean <JoshuaDean2023@u.northwestern.edu>
2024-11-07 13:56:13 -0500

    move .git directory

--- scripts/burak/brik2nii.sh
+++
@@ -1,9 +0,0 @@
-cd /data/akinb2/allbp/bp10/func_binh/
-3dAFNItoNIFTI  follow_ROI_aeseg+tlrc.BRIK.gz & 
-3dAFNItoNIFTI pb03*.BRIK &
-cd /data/akinb2/allbp/bp10/func_bouh/
-3dAFNItoNIFTI  follow_ROI_aeseg+tlrc.BRIK.gz & 
-3dAFNItoNIFTI pb03*.BRIK &
-cd /data/akinb2/allbp/bp10/func_rest/
-3dAFNItoNIFTI  follow_ROI_aeseg+tlrc.BRIK.gz & 
-3dAFNItoNIFTI pb03*.BRIK &
